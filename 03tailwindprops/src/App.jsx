895c5858688155058c3f552465198a994d87dbc4
Snehasish Das <78856467+snehasishdasz@users.noreply.github.com>
2024-10-30 00:45:38 +0530

    03-Tailwind-PROPS

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  const myObj={
+    name:"Snehasish",
+    age:22,
+    Gender:"Male",
+  }
+
+  return (
+    <>
+    <h1 className='bg-green-400 p-4 text-black rounded-l mb-4'>Tailwind Test</h1>
+    <Card  username="Bikash" btnText="ClickME"/>
+    <Card username="Dillip" btnText="VisitME"/>
+
+    </>
+  )
+}
+
+export default App
