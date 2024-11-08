628a08f92a01a78fdb96194048139629cd8b0f3b
Snehasish Das <78856467+snehasishdasz@users.noreply.github.com>
2024-11-09 00:04:25 +0530

    React Practice |  search part done

--- ReactAllPractice/src/App.jsx
+++ ReactAllPractice/src/App.jsx
@@ -1,34 +1,43 @@
 import { useEffect, useState } from "react";
 import Card from "./Card/Card";
+import ImageSearch from "./Card/ImageSearch";
 
 function App() {
-  const [images, setImages] = useState([]);  // Renamed to images for clarity
-  const [isLoading, setIsLoading] = useState(true);  // Fixed typo in isLoading
-  const [term, setTerm] = useState("");
+    const [images, setImages] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [term, setTerm] = useState(""); // This term will be set from ImageSearch input
 
-  const api = "29116599-a21a7b06895e86841053c6d9e";
+    const api = "29116599-a21a7b06895e86841053c6d9e";
 
-  useEffect(() => {
-    fetch(`https://pixabay.com/api/?key=${api}&q=${term}&image_type=photo&pretty=true`)
-      .then((res) => res.json())
-      .then((data) => {
-        setImages(data.hits);
-        setIsLoading(false);
-      })
-      .catch((err) => console.log(err));
-  }, [term]);
+    useEffect(() => {
+        if (term) {  // Only fetch images if there's a term
+            setIsLoading(true);
+            fetch(`https://pixabay.com/api/?key=${api}&q=${term}&image_type=photo&pretty=true`)
+                .then((res) => res.json())
+                .then((data) => {
+                    setImages(data.hits);
+                    setIsLoading(false);
+                })
+                .catch((err) => console.log(err));
+        }
+    }, [term]);
 
-  return (
-    <div className="container mx-auto">
-    {isLoading ? <h1 className="text-6xl text-center ">Loading....</h1> :
-      <div className="grid grid-cols-4 gap-3">
-        {images.map((image) => (
-          <Card key={image.id} image={image} />  // Added return statement here
-        ))}
-      </div>
-    }
-    </div>
-  );
+    const handleSearchSubmit = (searchTerm) => {
+        setTerm(searchTerm);  // Set the search term from ImageSearch
+    };
+
+    return (
+        <div className="container mx-auto">
+            <ImageSearch onSearchSubmit={handleSearchSubmit} />
+            {isLoading ? <h1 className="text-6xl text-center ">Loading....</h1> :
+                <div className="grid grid-cols-4 gap-3">
+                    {images.map((image) => (
+                        <Card key={image.id} image={image} />
+                    ))}
+                </div>
+            }
+        </div>
+    );
 }
 
 export default App;
